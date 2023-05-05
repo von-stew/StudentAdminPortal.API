564eff700bea035bba0b070f4d151385f9bafb48
von-stew <andrew.taylor.stewart@gmail.com>
2023-05-05 01:25:11 -0400

    End of Section 7

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -73,5 +73,51 @@ namespace StudentAdminPortal.API.Controllers
             return NotFound();
         }
 
+        [HttpPost]
+        [Route("[controller]/Add")]
+        public async Task<IActionResult> AddStudentAsync([FromBody] AddStudentRequest request)
+        {
+            var student = await studentRepository.AddStudent(mapper.Map<DataModels.Student>(request));
+            return CreatedAtAction(nameof(GetStudentAsync), new { studentId = student.Id },
+                mapper.Map<Student>(student));
+        }
+
+        //[HttpPost]
+        //[Route("[controller]/{studentId:guid}/upload-image")]
+        //public async Task<IActionResult> UploadImage([FromRoute] Guid studentId, IFormFile profileImage)
+        //{
+        //    var validExtensions = new List<string>
+        //    {
+        //       ".jpeg",
+        //       ".png",
+        //       ".gif",
+        //       ".jpg"
+        //    };
+
+        //    if (profileImage != null && profileImage.Length > 0)
+        //    {
+        //        var extension = Path.GetExtension(profileImage.FileName);
+        //        if (validExtensions.Contains(extension))
+        //        {
+        //            if (await studentRepository.Exists(studentId))
+        //            {
+        //                var fileName = Guid.NewGuid() + Path.GetExtension(profileImage.FileName);
+
+        //                var fileImagePath = await imageRepository.Upload(profileImage, fileName);
+
+        //                if (await studentRepository.UpdateProfileImage(studentId, fileImagePath))
+        //                {
+        //                    return Ok(fileImagePath);
+        //                }
+
+        //                return StatusCode(StatusCodes.Status500InternalServerError, "Error uploading image");
+        //            }
+        //        }
+
+        //        return BadRequest("This is not a valid Image format");
+        //    }
+
+        //    return NotFound();
+        //}
     }
 }
--- StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -20,6 +20,10 @@ namespace StudentAdminPortal.API.Profiles
 
             CreateMap<UpdateStudentRequest, DataModels.Student>()
                 .AfterMap<UpdateStudentRequestAfterMap>();
+
+
+            CreateMap<AddStudentRequest, DataModels.Student>()
+                .AfterMap<AddStudentRequestAfterMap>();
         }
     }
 }
